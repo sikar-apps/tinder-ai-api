90234b3347f2c79af6760528b9d2a280b092de5f
rohitsikarwar <sikar-apps@gmail.com>
2025-01-09 22:21:15 -0500

    Initial working configuration.

---
+++ src/main/java/com/sikar/tinderaiapi/TinderAiApiApplication.java
@@ -0,0 +1,13 @@
+package com.sikar.tinderaiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinderAiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sikar/tinderaiapi/TinderAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikar.tinderaiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
